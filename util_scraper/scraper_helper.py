4464ea98ae566cc93e5229b4a8c6feab0464aa34
bradyhhhuang <brady.huang@gogolook.com>
2023-07-02 16:05:27 +0800

    feature: add function notations

--- util_scraper/scraper_helper.py
+++ util_scraper/scraper_helper.py
@@ -13,41 +13,179 @@ from time import localtime, strftime
 
 
 class scraper:
-
-    def __init__(self, county_id, page=0, final_page=0):
+    '''
+        Scrapper can be used to scrape threads related to the house market on the mobile01 website.   
+    '''
+    
+    def __init__(self, county_id:str)->None:
+        '''
+            Specify the country_id of the country you want to parse. 
+        '''
+        
         self.county_id = county_id
         self.county_url = f'https://www.mobile01.com/topiclist.php?f={county_id}'
         self.save_path = f'scraped_result/{self.county_id}/'
         self.save_dt = strftime("%Y%m%d", localtime())
-        file_name = self.save_path + f'topic_summary_{self.save_dt}.pkl'
-        self.topic_dict = self.check_parsed_files(file_name)
+        self.__topic_summary_dict = {'url':[], 'title':[], 'first_post_time':[], 'last_post_time':[]}
+        self.__topic_content_dict = {'title':[], 'content':[], 'author':[], 'post_time':[], 'floor':[], 'cid':[]}
+    
+
+    def scrape_end2end(self)->None:
+        '''
+            Scrape threads of the county on the website end to end.
+        '''
+        # scrape topic summary and update self.topic_summary_dict
+        self.scrape_topic_summary()
+        topic_summary_df = self.topic_summary_df
         
-    def check_parsed_files(self, file_name):
-        if not os.path.exists(file_name):
-            topic_dict = {'url':[], 'title':[], 'first_post_time':[], 'last_post_time':[]}
-        else :
-            topic_df = pd.read_pickle(file_name)
-            topic_dict = topic_df.to_dict('list')
-        return topic_dict
-                
-    def scrape_topic_summary(self, final_page=0):
+        # remove duplications
+        topic_summary_df = topic_summary_df.sort_values(by='last_post_time', ascending=False)
+        topic_summary_df = topic_summary_df.reset_index(drop=True)
+        topic_summary_df = topic_summary_df[~topic_summary_df[['topic', 'url']].duplicated()]
+        
+        # scrape topic content and update self.topic_content_dict
+        self.scrape_topic_by_urls(topic_summary_df=topic_summary_df)
+    
+
+    def scrape_topic_by_urls(self, topic_summary_df:pd.DataFrame, file_name:str='None')->None:
+        '''
+            Scrape topic content by their urls. 
+        '''
+        
+        url_list = topic_summary_df['url'].tolist()
+        self.__topic_content_dict = self.check_parsed_files(file_name = file_name, file_type='topic_content')
+        
+        # Scrape topic content by its url.
+        for ind, url in enumerate(url_list):
+            
+            print('parsing: ', topic_summary_df.iloc[ind]['title'])
+            self.scrape_topic(url = url, topic_content_dict=self.__topic_content_dict)
+            
+    
+    def scrape_topic(self, url:str, topic_content_dict:dict, start_page:int=1)->dict:
+        '''
+            Scrape topic by pages and return topic_content_dict.
+        '''
         
         driver = self.create_driver()
-        # 
-        if final_page == 0:
-            url = self.county_url
-            
-            self.try_get_url(driver, url)
+        final_page = 1
+        
+        # Parse topic content from its first page to its final page.
+        while start_page <= final_page:
+            topic_url = f'https://www.mobile01.com/{url}&p={start_page}'
 
+            # start time
+            start = time.time()
+            
+            self.try_get_url(driver, topic_url)
             soup = BeautifulSoup(driver.page_source, 'html.parser')
+            
+            # If final page is not checked, then parse the final page of the threads of topic on the website.
+            if final_page == 1:
+                final_page = self.get_final_page(soup)
+            
+            # Parse topic content and update topic_content_dict
+            print(f'start to parse page {start_page}/{final_page}')
+            temp_dict = self.scrape_topic_content(soup)
+            for k, v in temp_dict.items():
+                self.__topic_content_dict[k] += v
+            
+            # end time
+            end = time.time()
+            sec = end - start
+            print(f"Time spend: {sec:.2f} seconds")
+            
+            # save scraped result
+            self.save_dt = strftime("%Y%m%d", localtime())
+            if not os.path.isdir(self.save_path):
+                os.makedirs(self.save_path)
+            save_file_name = self.save_path + f'topic_content_{self.save_dt}.pkl'
+            content_df = pd.DataFrame(self.__topic_content_dict)
+            content_df.to_pickle(save_file_name)
+            
+            start_page +=1
+            
             print('Start to sleep')
-            self.sleep(5, random=True)
+            self.sleep(5, random_time=True)
+            print('End of sleeping')
+            
+        driver.close()
+        
+    
+    def scrape_topic_content(self, soup)->dict:
+        '''
+            Parse contents in a thread and scrape the contents.
+        '''
+        
+        topic_content_dict = {'title':[], 'content':[], 'author':[], 'post_time':[], 'floor':[], 'cid':[]}
+        title = soup.find('h1', {'class':'t2'}).text.strip()
+        contents = soup.find_all("div", {"class": "l-articlePage"})
+        
+        # parse content by floors
+        for c in contents:
+            # find author
+            author = c.find('div', {'class':'c-authorInfo__id'})
+            if not author:
+                continue
+            topic_content_dict['author'].append(author.text.strip())
+            
+            # find post_time and floor
+            navigation = c.find(lambda tag: tag.name == 'div' and 
+                                   tag.get('class') == ['l-navigation__item'])
+            topic_content_dict['post_time'].append(navigation.find_all('span', {'class':'o-fNotes o-fSubMini'})[0].text.strip())
+            floor = navigation.find_all('span', {'class':'o-fNotes o-fSubMini'})[1].text.replace('#', '').strip()
+            topic_content_dict['floor'].append(floor)
+            
+            # find content 
+            all_text = c.find('article').get_text(' ')
+            # remove quotation
+            quote_list = c.find('article').find_all('blockquote')
+            for quote in quote_list:
+                # replace <\br> to ' '
+                quote_text = quote.get_text(' ')
+                all_text = all_text.replace(quote_text, '')
+            topic_content_dict['content'].append(all_text.strip())
+            topic_content_dict['cid'].append(self.county_id)
+            topic_content_dict['title'].append(title)
+            
+            # check whether there are replies in a floor
+            appear_area = c.find('div', {'class':'l-appearArea'})
+            if appear_area:
+                replies = appear_area.find_all('div', {'class':'l-leaveMsg__msg'})
+                for ind, r in enumerate(replies):
+                    topic_content_dict['author'].append(r.find('a', {'class':'c-link c-link--gn u-username'}).text.strip())
+                    topic_content_dict['post_time'].append(r.find('span', {'class':'o-fSubMini o-fNotes'}).text.strip())
+                    topic_content_dict['floor'].append(floor+'-'+str(ind+1))
+                    topic_content_dict['content'].append(r.find('div', {'class':'msgContent c-summary__desc'}).get_text(' ').strip())
+                    topic_content_dict['cid'].append(self.county_id)
+                    topic_content_dict['title'].append(title)
+        return topic_content_dict
+
+
+    def scrape_topic_summary(self, final_page=0, file_name:str='None')->None:
+        '''
+            Create a driver, scrape topic summaries and update self.topic_summary_dict.
+        '''
+        
+        driver = self.create_driver()
+        self.__topic_summary_dict = self.check_parsed_files(file_name = file_name, file_type='topic_summary')
+
+        # If final page is not specified, then parse the final page of the threads of county on the website.
+        if final_page == 0:
+            
+            url = self.county_url
+            self.try_get_url(driver, url)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            
+            print('Start to sleep')
+            self.sleep(5, random_time=True)
             print('End of sleeping')
         
-            # Check final page
+            # parse final page
             final_page = self.get_final_page(soup)
             print('End of parsing final_page')
             
+        # Scrape the topic summary from final page to the first page
         for p in range(1, final_page+1)[::-1]:
             print(f'Go to page {p}')
             
@@ -57,124 +195,90 @@ class scraper:
             page_url = self.county_url + f'&p={p}'
             self.try_get_url(driver, page_url)
             soup = BeautifulSoup(driver.page_source, 'html.parser')
-            self.topic_dict = self.get_topic_urls(soup)
+            self.__topic_summary_dict = self.get_topic_summary(soup)
+            
             # end time
             end = time.time()
             sec = end - start
             print(f"Time spend: {sec:.2f} seconds")
             
             # save scraped result
-            
             self.save_dt = strftime("%Y%m%d", localtime())
             if not os.path.isdir(self.save_path):
                 os.makedirs(self.save_path)
             file_name = self.save_path + f'topic_summary_{self.save_dt}.pkl'
-            pd.DataFrame(self.topic_dict).to_pickle(file_name)
+            pd.DataFrame(self.__topic_summary_dict).to_pickle(file_name)
             
-            self.page = p
             print('Start to sleep')
-            self.sleep(1, random=True)
+            self.sleep(5, random_time=True)
             print('End of sleeping')
+            
         driver.close()
-        
-    def scrape_topic_url(self, topic_df):
-        topic_urls = topic_df['url'].tolist()
-        for ind, url in enumerate(topic_urls):
-            print('parsing: ', topic_df.iloc[ind]['title'])
-            content_dict = self.scrape_topic(url = url, cid = self.county_id)
-            
-            self.save_dt = strftime("%Y%m%d", localtime())
-            filename = self.save_path + f'topic_{self.save_dt}.pkl'
-            content_df = pd.DataFrame(content_dict)
-            content_df['cid'] = self.county_id
-            content_df.to_pickle(filename)
     
-    def scrape_topic(self, url, cid, start_page=1, content_dict = {}):
-        driver = self.create_driver()
-        final_page = 1
-        if len(content_dict) == 0:
-            content_dict = {'title':[], 'content':[], 'author':[], 'post_time':[], 'floor':[], 'post_time':[], 'cid':[]}
-        
-        while start_page <= final_page:
-            print(f'start to parse page {start_page}')
-            topic_url = f'https://www.mobile01.com/{url}&p={start_page}'
 
-            # start time
-            start = time.time()
-            
-            self.try_get_url(driver, topic_url)
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            
-            if final_page == 1:
-                final_page = self.get_final_page(soup)
+    def get_topic_summary(self, soup)->dict:
+        '''
+            Get topic summary and update self.topic_summary_dict.
+        '''
+        topic_summary_dict = self.__topic_summary_dict
+        table = soup.find_all("div", {"class": "l-listTable__tbody"})[0]
+        temp_topic = table.find_all('a', {'class': 'c-link u-ellipsis'})
+        temp_time = table.find_all('div', {'class': 'o-fNotes'})
+        for ind in range(len(temp_topic)):
+            topic_summary_dict['url'].append(temp_topic[ind].get('href'))
+            topic_summary_dict['title'].append(temp_topic[ind].text)
+            topic_summary_dict['first_post_time'].append(temp_time[ind*2].text)
+            topic_summary_dict['last_post_time'].append(temp_time[ind*2+1].text)
+        return topic_summary_dict
 
-            temp_dict = self.scrape_topic_content(soup)
-            print(temp_dict)
-            for k, v in temp_dict.items():
-                content_dict[k] += v
-            
-            # end time
-            end = time.time()
-            sec = end - start
-            print(f"Time spend: {sec:.2f} seconds")
-            
-            self.save_dt = strftime("%Y%m%d", localtime())
-            filename = self.save_path + f'topic_{self.save_dt}.pkl'
-            content_df = pd.DataFrame(content_dict)
-            print(content_df.shape)
-            content_df.to_pickle(filename)
-            start_page +=1
-            print('Start to sleep')
-            self.sleep(3, random=True)
-            print('End of sleeping')
-        driver.close()
+
+    def get_final_page(self, soup)->int:
+        '''
+            Find the final page in a thread.
+        '''
+        page_list = soup.find_all("a", {"class": "c-pagination"})
         
-        return content_dict
-    
-    def scrape_topic_content(self, soup):
-        content_dict = {'title':[], 'content':[], 'author':[], 'post_time':[], 'floor':[], 'cid':[]}
-        title = soup.find('h1', {'class':'t2'}).text.strip()
-        contents = soup.find_all("div", {"class": "l-articlePage"})
-        
-        for c in contents:    
-            author = c.find('div', {'class':'c-authorInfo__id'})
-            if not author:
-                continue
-            content_dict['author'].append(author.text.strip())
-            navigation = c.find(lambda tag: tag.name == 'div' and 
-                                   tag.get('class') == ['l-navigation__item'])
-            content_dict['post_time'].append(navigation.find_all('span', {'class':'o-fNotes o-fSubMini'})[0].text.strip())
-            floor = navigation.find_all('span', {'class':'o-fNotes o-fSubMini'})[1].text.replace('#', '').strip()
-            content_dict['floor'].append(floor)
-            all_text = c.find('article').get_text(' ')
-            quote_list = c.find('article').find_all('blockquote')
-            for quote in quote_list:
-                quote_text = quote.get_text(' ')
-                all_text = all_text.replace(quote_text, '')
-            content_dict['content'].append(all_text.strip())
-            content_dict['cid'].append(self.county_id)
-            content_dict['title'].append(title)
+        if len(page_list) == 0:
+            final_page = '1'
+        else :
+            final_page = page_list[-1].text
             
-            appear_area = c.find('div', {'class':'l-appearArea'})
-            if appear_area:
-                replies = appear_area.find_all('div', {'class':'l-leaveMsg__msg'})
-                for ind, r in enumerate(replies):
-                    content_dict['author'].append(r.find('a', {'class':'c-link c-link--gn u-username'}).text.strip())
-                    content_dict['post_time'].append(r.find('span', {'class':'o-fSubMini o-fNotes'}).text.strip())
-                    content_dict['floor'].append(floor+'-'+str(ind+1))
-                    content_dict['content'].append(r.find('div', {'class':'msgContent c-summary__desc'}).get_text(' ').strip())
-                    content_dict['cid'].append(self.county_id)
-                    content_dict['title'].append(title)
-        return content_dict
+        return int(final_page)
     
+
+    def check_parsed_files(self, file_name:str, file_type:str)->dict:
+        '''
+            Check whether to use self.topic_summary_dict/self.topic_content_dict or not.
+        '''
+        
+        file_name = self.save_path + file_name
+        
+        if file_type == 'topic_summary':
+            if not os.path.exists(file_name):
+                result_dict = self.__topic_summary_dict
+            else :
+                result_df = pd.read_pickle(file_name)
+                result_dict = result_df.to_dict('list')
+                
+        elif file_type == 'topic_content':
+            if not os.path.exists(file_name):
+                result_dict = self.__topic_content_dict
+            else :
+                result_df = pd.read_pickle(file_name)
+                result_dict = result_df.to_dict('list')
+        else:
+            raise ValueError("file_type can only be either 'topic_summary' or 'topic_content'.")
+        
+        return result_dict
+    
+
     def create_driver(self):
-        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
-        prefs = {"profile.managed_default_content_settings.images": 2}
+        '''
+            Create a driver with option arguments.
+        '''
         
         opt = uc.ChromeOptions() 
-        opt.add_experimental_option("prefs", prefs)
         opt.add_argument('--disable-gpu')
-        opt.add_argument('--user-agent=%s' % user_agent)
         opt.add_argument("--disable-notifications")
         opt.add_argument("--disable-extensions")
         opt.add_argument("--disable-blink-features=AutomationControlled")
@@ -183,45 +287,51 @@ class scraper:
         
         return driver
 
-    def try_get_url(self, driver, url):
+
+    def try_get_url(self, driver, url:str)->None:
+        '''
+            Get url by driver. Try again after 300s if it fails. 
+        '''
         try: 
             driver.get(url)
         except:
             print('Fail to get url, will try to get again in 300s.')
-            self.sleep(300, random=False)
+            self.sleep(300, random_time=False)
             driver.get(url)
 
-    def get_topic_urls(self, soup):
-        topic_dict = self.topic_dict
-        table = soup.find_all("div", {"class": "l-listTable__tbody"})[0]
-        temp_topic = table.find_all('a', {'class': 'c-link u-ellipsis'})
-        temp_time = table.find_all('div', {'class': 'o-fNotes'})
-        print(temp_time)
-        for ind in range(len(temp_topic)):
-            topic_dict['url'].append(temp_topic[ind].get('href'))
-            topic_dict['title'].append(temp_topic[ind].text)
-            topic_dict['first_post_time'].append(temp_time[ind*2].text)
-            topic_dict['last_post_time'].append(temp_time[ind*2+1].text)
-        return topic_dict
-        
 
-    def get_final_page(self, soup):
-        page_list = soup.find_all("a", {"class": "c-pagination"})
-        
-        if len(page_list) == 0:
-            final_page = '1'
-        else :
-            final_page = page_list[-1].text
-        return int(final_page)
-
-    def sleep(self, t, random=True):
+    def sleep(self, t, random_time=True)->None:
+        '''
+            Sleep for {t} + random seconds to avoid getting banned.
+        '''
         if random:
             r = random.random()
             if r<=0.5:
-                time.sleep(t+random.random()*5)
+                time.sleep(t+random.random()*10)
             elif r<=0.9:
                 time.sleep(t+random.random()*20)
             else:
                 time.sleep(t+random.random()*50)
         else:
             time.sleep(t)
+
+
+    @property
+    def topic_summary_df(self):
+        return pd.DataFrame(self.__topic_summary_dict)
+    
+
+    @property
+    def topic_content_df(self):
+        return pd.DataFrame(self.__topic_content_dict)
+    
+
+    @property
+    def topic_summary_dict(self):
+        return self.__topic_summary_dict
+    
+    
+    @property
+    def topic_content_dict(self):
+        return self.__topic_content_dict
+    
